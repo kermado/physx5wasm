07484bb9dc93a42768655c839717d0866f16cb2c
Omar Kermad <homar@me.com>
2023-07-12 22:30:53 +0100

    Build release, debug, checked and profile configurations.

--- make.sh
+++ make.sh
@@ -2,3 +2,12 @@
 
 cd ./physx/compiler/emscripten-release/
 make -j$(nproc)
+
+cd ../emscripten-debug
+make -j$(nproc)
+
+cd ../emscripten-checked
+make -j$(nproc)
+
+cd ../emscripten-profile
+make -j$(nproc)
